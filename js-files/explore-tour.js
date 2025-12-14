f043a7d7a683af62970bebc975b950908b8a3990
Eniola Sulaiman <ennysulaiman2011@gmail.com>
2025-12-14 12:12:34 +0100

    logic:refactor code for loading

--- js-files/explore-tour.js
+++ js-files/explore-tour.js
@@ -54,37 +54,10 @@ async function loadRoute() {
     const params = new URLSearchParams(window.location.search);
     const tour = params.get(`tour`);
 
-    document.getElementById(`toursMain`).innerHTML = `
-<div class="container card">
-                    <img id="image" src="${data[3][tour].img1}" alt="Destination Image">
-                    <img id="image" src="${data[3][tour].img2}" alt="Destination Image">
-                    <img id="image" src="${data[3][tour].img3}" alt="Destination Image">
-                    <span id="prevBtn" class="prev-btn" onclick="prevSlide()">&#10094;</span>
-                    <span id="nextBtn" class="next-btn" onclick="nextSlide()">&#10095;</span>
-                    <span class="h1-container flex">
-                        <h1>${data[3][tour].name}</h1>
-                    </span>
-                    <div class="text">
-                        <span class="about-text">
-                            <h2>About the Tour</h2>
-                        </span>
-                        <p>${data[3][tour].desc}</p>
-                        <div class="grid-3">
-                            <span>
-                                <h3>Price</h3>
-                                ${data[3][tour].price}
-                            </span>
-                            <span>
-                                <h3>Duration</h3>
-                                ${data[3][tour].duration}
-                            </span>
-                            <span>
-                                <a href="#" class="btn">Book Now</a>
-                            </span>
-                        </div>
-                    </div>
-                </div>
-                `;
+    document.getElementById(`tourName`).innerHTML=data[3][tour].name
+    document.getElementById(`tourAbout`).innerHTML=data[3][tour].desc
+    document.getElementById(`tourDuration`).innerHTML=data[3][tour].
+    document.getElementById(`tourPrice`).innerHTML=data[3][tour].pricePerNight * Number(tourDuration.value.split(` `)[0])
   } catch (error) {
     console.error(`Failed to load data for route`, error);
   }
