d465c2c61bcdf6065c18120053bc9101cf4ba917
Eniola Sulaiman <ennysulaiman2011@gmail.com>
2025-12-31 16:59:14 +0100

    chore:remove unused and ommented out code

--- js-files/index.js
+++ js-files/index.js
@@ -10,32 +10,3 @@ function showMenu() {
     })
 }
 showMenu()
-
-/* function sliderHover() {
-    const images = document.querySelectorAll(`#slides`)
-    const destinations = document.querySelectorAll(`#destinations`)
-    const destinationName = document.querySelectorAll(`#destinationName`)
-    
-
-    for(let i = 0;  i < destinations.length; i++ ){
-        const currentImage = images[i]
-        const currentDestination = destinations[i]
-        const currentDestinationName = destinationName[i]
-        currentDestination.addEventListener(`mouseover`, () => {
-            currentDestinationName.style.opacity = `1`
-            currentImage.style.transform = `scale(1.1)`
-            currentDestination.style.width = `7.7em`
-            currentDestination.style.height = `11em`
-        })
-
-        currentDestination.addEventListener(`mouseout`, () => {
-            currentDestinationName.style.opacity = `0`
-            currentImage.style.transform = `scale(1)`
-            currentDestination.style.width = `min-content`
-            currentDestination.style.height = `min-content`
-        })
-
-    }
-
-}
-sliderHover() */
